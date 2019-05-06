348075e63e4e79be1b6fb774936e016bfb4d1aac
unknown <s5704062636489@email.kmutnb.ac.th>
2019-05-06 19:49:28 +0700

    admin

---
+++ resource/templates/admin/asset/js/adminEvent.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function () {
+    $("#menu-addUser").hide()
+
+    $("#managementLink").click(function () {
+      
+        if ($('#adminManagement ul').length == 0) {
+            $("#adminManagement").append(`<ul id="childAdminManagement"class="sub-nav-link">
+            <a href="#" id="staff" onclick="$.fn.contentStaff()">STAFF</a>
+            </ul>`);
+         
+        }else{
+            $("#childAdminManagement").remove()
+        }
+    });
+    
+    $("#accountLink").click(function(){
+        $("#menu-addUser").hide()
+        $.fn.contentAccount()
+    });
+
+});
